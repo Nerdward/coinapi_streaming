6f1d4c47dd094c739c3939c8e5d7288812bb7742
Ohakim Nnaemeka <ohakim.nnaemeka@gmail.com>
2022-07-07 09:56:16 +0000

    changed the column dtypes and can read to parquet

--- spark-streaming/test.py
+++ spark-streaming/test.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
+from pyspark.sql import functions as F
 from pyspark.sql.functions import from_json, col
 
 
@@ -35,9 +36,28 @@ if __name__ == '__main__':
     assets_df = df.select(from_json(col('value'), df_schema).alias('data'))\
                 .select('data.*')
 
-    assets_df.writeStream \
-    .outputMode('update')\
-    .format("console")\
-    .start()\
-    .awaitTermination()
+    assets_df = assets_df.withColumn('rank', assets_df['rank'].cast('int'))\
+                        .withColumn('maxSupply', assets_df['maxSupply'].cast('float'))\
+                        .withColumn('marketCapUsd', assets_df['marketCapUsd'].cast('float'))\
+                        .withColumn('volumeUsd24Hr', assets_df['volumeUsd24Hr'].cast('float'))\
+                        .withColumn('priceUsd', assets_df['priceUsd'].cast('float'))\
+                        .withColumn('changePercent24hr', assets_df['changePercent24hr'].cast('double'))\
+                        .withColumn('vwap24Hr', assets_df['vwap24Hr'].cast('float'))
+                        
+                        
+                    
 
+    # assets_df.writeStream \
+    # .outputMode('update')\
+    # .format("console")\
+    # .start()\
+    # .awaitTermination()
+
+    assets_df.writeStream \
+            .format('parquet')\
+            .option("path", './storage')\
+            .option("checkpointLocation", './checkpoint')\
+            .trigger(processingTime='60 seconds')\
+            .outputMode('append')\
+            .start()\
+            .awaitTermination()
